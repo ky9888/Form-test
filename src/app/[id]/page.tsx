4d9a863e7cff12ddfe663190d9c9925e9f860e8b
ky <nvk250698@gmail.com>
2025-01-04 09:55:46 +0700

    update9

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,10 +11,24 @@ export type SelectImgType = {
   image: string;
 };
 
+export async function generateStaticParams() {
+  // Fetch danh sách sản phẩm để tạo các tham số động
+  const res = await fetch(
+    "https://be-e-commerce-tohe.onrender.com/api/products/getAllProducts"
+  );
+  const result = await res.json();
+  const products: ProductType[] = result.data;
+
+  // Trả về danh sách params để Next.js pre-render
+  return products.map((product) => ({
+    id: product._id,
+  }));
+}
+
 export default async function Home({
   params,
 }: {
-  params: { id: string }; 
+  params: { id: string }; // Kiểu dữ liệu phù hợp với Next.js
 }) {
   let data: ProductType | null = null;
 
