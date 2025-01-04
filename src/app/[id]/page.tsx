96144f6c7aad86de33f2d4a9586d89ec88777b5e
ky <nvk250698@gmail.com>
2025-01-04 10:08:46 +0700

    update10

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,30 +11,17 @@ export type SelectImgType = {
   image: string;
 };
 
-export async function generateStaticParams() {
-  // Fetch danh sách sản phẩm để tạo các tham số động
-  const res = await fetch(
-    "https://be-e-commerce-tohe.onrender.com/api/products/getAllProducts"
-  );
-  const result = await res.json();
-  const products: ProductType[] = result.data;
-
-  // Trả về danh sách params để Next.js pre-render
-  return products.map((product) => ({
-    id: product._id,
-  }));
-}
-
 export default async function Home({
   params,
 }: {
-  params: { id: string }; // Kiểu dữ liệu phù hợp với Next.js
+  params: Promise<{ id: string }>
 }) {
+  const id = (await params).id
   let data: ProductType | null = null;
 
   try {
     const res = await fetch(
-      `https://be-e-commerce-tohe.onrender.com/api/products/getDetailProducts/${params.id}`,
+      `https://be-e-commerce-tohe.onrender.com/api/products/getDetailProducts/${id}`,
       { cache: "no-store" }
     );
     const result = await res.json();
